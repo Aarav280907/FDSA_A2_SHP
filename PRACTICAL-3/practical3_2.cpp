b5ec5a70be9262a8f76a2492784a82a852f2f878
AARAV PATEL <patelaarav1512@gmail.com>
2026-08-03 13:43:54 +0530

    Update practical3_2.cpp

--- PRACTICAL-3/practical3_2.cpp
+++ PRACTICAL-3/practical3_2.cpp
@@ -3,3 +3,34 @@
 Given the list of colour codes, rearrange them in place and print the result.*/
 
 #include<iostream>
+#include<vector>
+
+using namespace std;
+
+void insertionsort(vector<int>& arr)
+{
+    int n=arr.size();
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+int main()
+{
+    vector<int> colors={2,0,1,2,1,0,0,2,1,0,2};
+    insertionsort(colors);
+    cout<<"Sorted Colors:";
+    for(int i=0;i<colors.size();i++)
+    {
+        cout<<colors[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
