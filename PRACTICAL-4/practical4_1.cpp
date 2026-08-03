936d03d92e8da55ab526bcab2c476fda6b90fbb4
AARAV PATEL <patelaarav1512@gmail.com>
2026-08-03 14:02:07 +0530

    Create practical4_1.cpp

---
+++ PRACTICAL-4/practical4_1.cpp
@@ -0,0 +1,3 @@
+/*A hospital manages a queue of patient tokens. New critical patients must be added to the front, routine patients are added to the end, 
+and occasionally a patient with a priority number must be inserted at a specific position in the queue. Given a sequence of such operations,
+ implement all three insertion types and print the final queue after each operation.*/
